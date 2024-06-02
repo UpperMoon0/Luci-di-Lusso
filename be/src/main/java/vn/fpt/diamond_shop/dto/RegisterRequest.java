dd35eb25a74d67d787527f016a0eac3abd7874cc
Nhat <60876500+UpperMoon0@users.noreply.github.com>
2024-06-02 22:51:35 +0700

    Nhat - Updated RegisterRequest constraints

--- be/src/main/java/vn/fpt/diamond_shop/dto/RegisterRequest.java
+++ be/src/main/java/vn/fpt/diamond_shop/dto/RegisterRequest.java
@@ -3,22 +3,25 @@ package vn.fpt.diamond_shop.dto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import vn.fpt.diamond_shop.constant.EAuthProvider;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
 @ToString
 public class RegisterRequest {
-    @NotBlank
+    @Pattern(regexp = "^[a-zA-Z0-9]*$",
+            message = "Username can only contain letters and numbers")
     private String username;
 
-    @NotBlank
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",
+            message = "Password must contain at least one number, one lowercase letter, one uppercase letter, one special character, and have a minimum length of 8 characters")
     private String password;
 
-    @NotBlank
     @Email
     private String email;
 
@@ -26,5 +29,5 @@ public class RegisterRequest {
     private String fullName;
 
     @NotNull
-    private String provider;
+    private EAuthProvider provider;
 }
