a1314bac9fb4d27e0022d8c3735ed3ab5a688e41
_lhk_1615 <114357364+Khang151603@users.noreply.github.com>
2024-06-09 21:57:54 +0700

    update Fe

---
+++ fe/src/app/account/login.component.ts
@@ -0,0 +1,90 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AccountService, AlertService  } from "../_services";
+// import * as $ from 'jquery';
+
+declare var $: any;
+@Component({ templateUrl: 'login.component.html' })
+export class LoginComponent implements OnInit {
+    form!: FormGroup;
+    loading = false;
+    submitted = false;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private accountService: AccountService,
+        private alertService: AlertService
+    ) { }
+
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.form.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // reset alerts on submit
+        this.alertService.clear();
+
+        // stop here if form is invalid
+        if (this.form.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.accountService.login(this.f['username'].value, this.f['password'].value)
+            .pipe(first())
+            .subscribe({
+                next: () => {
+                    this.showNotification2(2,"Login thành công",'top','right');
+                    // get return url from query parameters or default to home page
+                    const returnUrl = this.route.snapshot.queryParams['/dashboard'] || '/dashboard';
+                    this.router.navigateByUrl(returnUrl);
+                },
+                error: error => {
+                    this.showNotification2(4,"Login thất bại",'top','right');
+                    this.alertService.error(error);
+                    this.loading = false;
+                }
+            });
+    }
+    showNotification2(kind: any,mess: any,from: any , align: any){
+        const type = ['','info','success','warning','danger'];
+
+        // const color = Math.floor((Math.random() * 4) + 1);
+
+        $.notify({
+            icon: "notifications",
+            message: mess,
+
+        },{
+            type: type[kind],
+            timer: 4000,
+            placement: {
+                from: from,
+                align: align
+            },
+            template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
+                '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
+                '<i class="material-icons" data-notify="icon">notifications</i> ' +
+                '<span data-notify="title">{1}</span> ' +
+                '<span data-notify="message">{2}</span>' +
+                '<div class="progress" data-notify="progressbar">' +
+                '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+                '</div>' +
+                '<a href="{3}" target="{4}" data-notify="url"></a>' +
+                '</div>'
+        });
+    }
+}
