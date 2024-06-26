2c652d00d2e6b10c84559814a3c57b38f4a683db
UpperMoon0 <60876500+UpperMoon0@users.noreply.github.com>
2024-06-27 01:18:45 +0700

    Added missing annotations for Repositories and Entities

--- be/src/main/java/vn/fpt/diamond_shop/model/entity/Receipt.java
+++ be/src/main/java/vn/fpt/diamond_shop/model/entity/Receipt.java
@@ -1,18 +1,14 @@
 package vn.fpt.diamond_shop.model.entity;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
+@Data
 @Entity
 @Table(name = "receipt")
 public class Receipt {
-    @Setter(value = AccessLevel.NONE)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -22,6 +18,9 @@ public class Receipt {
 
     @Column(name = "total_price")
     private Double totalPrice;
+
+    @Column(name = "confirmed")
+    private Boolean confirmed;
 
     @Column(name = "create_at")
     private LocalDateTime createAt;
--- be/src/main/java/vn/fpt/diamond_shop/repository/IJewelryRepository.java
+++ be/src/main/java/vn/fpt/diamond_shop/repository/IJewelryRepository.java
@@ -3,10 +3,12 @@ package vn.fpt.diamond_shop.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import vn.fpt.diamond_shop.model.entity.Jewelry;
 
 import java.util.List;
 
+@Repository
 public interface IJewelryRepository extends JpaRepository<Jewelry, Long> {
     @Query("SELECT SUM(j.price) FROM Jewelry j WHERE j.id IN (:ids)")
     Double getTotalPriceByIdList(@Param("ids") List<Long> ids);
