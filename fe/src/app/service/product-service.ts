4d2ca3aca23e288becbb5bfab81ed773ab9f4284
UpperMoon0 <60876500+UpperMoon0@users.noreply.github.com>
2024-07-03 20:31:04 +0700

    Small fix

--- fe/src/app/service/product-service.ts
+++ fe/src/app/service/product-service.ts
@@ -10,15 +10,7 @@ export class ProductService {
   constructor(private http: HttpClient, private router: Router) { }
 
   getJewelry(id: number): Observable<any> {
-    // Retrieve the JWT token from local storage
-    const token = localStorage.getItem('accessToken');
-
-    // Create HttpHeaders and set Authorization header
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`
-    });
-
     // Pass headers as an option to the http.get method
-    return this.http.get('http://localhost:8081/product/get-jewelry?id=' + id, { headers });
+    return this.http.get('http://localhost:8081/product/get-jewelry?id=' + id);
   }
 }
