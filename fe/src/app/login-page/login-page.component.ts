f71c01642303036347cf175ca59028c7104dcf0d
UpperMoon0 <60876500+UpperMoon0@users.noreply.github.com>
2024-07-08 02:14:31 +0700

    Routing test for static web app

--- fe/src/app/login-page/login-page.component.ts
+++ fe/src/app/login-page/login-page.component.ts
@@ -1,9 +1,4 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {ActivatedRoute, Router} from "@angular/router";
-import {AccountService} from "../modules/auth/services/account.service";
-import {AuthGoogleService} from "../core/shared/auth-google.service";
-import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'login-page',
