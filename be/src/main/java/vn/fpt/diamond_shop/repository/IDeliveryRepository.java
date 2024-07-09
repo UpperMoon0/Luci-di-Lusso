7bd2042eb3657455bafb4c83161e4da5a388dc26
Nhat <60876500+UpperMoon0@users.noreply.github.com>
2024-07-09 14:30:31 +0700

    Create IDeliveryRepository.java

---
+++ be/src/main/java/vn/fpt/diamond_shop/repository/IDeliveryRepository.java
@@ -0,0 +1,10 @@
+package vn.fpt.diamond_shop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import vn.fpt.diamond_shop.model.entity.Delivery;
+
+@Repository
+public interface IDeliveryRepository extends JpaRepository<Delivery, Long>
+
+}
