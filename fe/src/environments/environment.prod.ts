e1f72598edbb17917f4bff150297b89b8676e791
Vo Nguyen Minh Nhat <60876500+UpperMoon0@users.noreply.github.com>
2024-07-13 13:58:19 +0700

    Update environment.prod.ts

--- fe/src/environments/environment.prod.ts
+++ fe/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  api_url:'http://localhost:3000/api/v1/',
-  ws_url:'http://localhost:3000',
+  api_url:'https://ambitious-glacier-031a9ac00.5.azurestaticapps.net:3000/api/v1/',
+  ws_url:'https://ambitious-glacier-031a9ac00.5.azurestaticapps.net:3000',
   companyId:'1254863933218p0ppp',
-  beApiUrl: 'http://localhost:8080',
-  pageApi:'http://localhost:3000',
+  beApiUrl: 'https://luci-di-lusso-be.livelyforest-407b0308.southeastasia.azurecontainerapps.io:8080',
+  pageApi:'https://ambitious-glacier-031a9ac00.5.azurestaticapps.net:3000',
 };
