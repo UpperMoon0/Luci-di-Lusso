8d9ff3adff7ef09d53e513ae7e736bcf668d286f
UpperMoon0 <60876500+UpperMoon0@users.noreply.github.com>
2024-07-22 01:40:34 +0700

    Diamond crud completed 2

--- fe/src/app/manager-page/diamond-edit.component.ts
+++ fe/src/app/manager-page/diamond-edit.component.ts
@@ -11,18 +11,18 @@ import {ToastrService} from "ngx-toastr";
   styleUrls: ['./diamond-edit.component.css']
 })
 export class DiamondEditComponent implements OnInit {
-  protected diamondProperties: any;
-  protected selectedCut: any = '';
-  protected selectedColor: any = '';
-  protected selectedClarity: any = '';
-  protected selectedShape: any = '';
+  protected diamondProperties: any = {};
+  protected initialCut: any = {};
+  protected initialColor: any = {};
+  protected initialClarity: any = {};
+  protected initialShape: any = {};
 
   diamondEditForm = new FormGroup({
-    cut: new FormControl(this.selectedCut.id),
-    color: new FormControl(this.selectedColor.id),
-    clarity: new FormControl(this.selectedClarity.id),
+    cut: new FormControl(this.initialCut),
+    color: new FormControl(this.initialColor),
+    clarity: new FormControl(this.initialClarity),
     carat: new FormControl(this.data.diamond.carat),
-    shape: new FormControl(this.selectedShape.id),
+    shape: new FormControl(this.initialShape),
     quantity: new FormControl(this.data.diamond.quantity),
   });
 
@@ -37,35 +37,32 @@ export class DiamondEditComponent implements OnInit {
     this.productService.getAllDiamondProperties().subscribe(response => {
       this.diamondProperties = response;
 
-      // Set selected values
-      this.diamondProperties.cuts.forEach((cut: any) => {
-        if (cut.cut === this.data.diamond.cut) {
-          this.selectedCut = cut;
-        }
-      });
+      // Set selected values using find
+      this.initialCut = this.diamondProperties.cuts.find((cut: any) => cut.cut === this.data.diamond.cut);
+      this.initialColor = this.diamondProperties.colors.find((color: any) => color.color === this.data.diamond.color);
+      this.initialClarity = this.diamondProperties.clarities.find((clarity: any) => clarity.clarity === this.data.diamond.clarity);
+      this.initialShape = this.diamondProperties.shapes.find((shape: any) => shape.shape === this.data.diamond.shape);
 
-      this.diamondProperties.colors.forEach((color: any) => {
-        if (color.color === this.data.diamond.color) {
-          this.selectedColor = color;
-        }
-      });
-
-      this.diamondProperties.clarities.forEach((clarity: any) => {
-        if (clarity.clarity === this.data.diamond.clarity) {
-          this.selectedClarity = clarity;
-        }
-      });
-
-      this.diamondProperties.shapes.forEach((shape: any) => {
-        if (shape.shape === this.data.diamond.shape) {
-          this.selectedShape = shape;
-        }
+      // Update form controls with the found values
+      this.diamondEditForm.patchValue({
+        cut: this.initialCut,
+        color: this.initialColor,
+        clarity: this.initialClarity,
+        shape: this.initialShape,
       });
     });
   }
 
   onSubmit(): void {
-    let diamond = {id: this.data.diamond.id, clarityId: this.selectedClarity.id, colorId: this.selectedColor.id, cutId: this.selectedCut.id, shapeId: this.selectedShape.id, carat: this.diamondEditForm.value.carat, quantity: this.diamondEditForm.value.quantity};
+    console.log(this.diamondProperties);
+    console.log(JSON.stringify(this.diamondEditForm.value.clarity));
+    let diamond = {id: this.data.diamond.id,
+                                            clarityId: this.diamondEditForm.value.clarity.id,
+                                            colorId: this.diamondEditForm.value.color.id,
+                                            cutId: this.diamondEditForm.value.cut.id,
+                                            shapeId: this.diamondEditForm.value.shape.id,
+                                            carat: this.diamondEditForm.value.carat,
+                                            quantity: this.diamondEditForm.value.quantity};
     this.managerService.updateDiamond(diamond).subscribe(
       () => {
         this.toastrService.success('Diamond updated successfully');
