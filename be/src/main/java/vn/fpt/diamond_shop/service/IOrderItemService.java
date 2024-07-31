f3719707187fd4ad6f78bd9002f48c3326daa27b
UpperMoon0 <60876500+UpperMoon0@users.noreply.github.com>
2024-07-31 14:33:48 +0700

    Fix order creating without items and purchase history incorrect display

--- be/src/main/java/vn/fpt/diamond_shop/service/IOrderItemService.java
+++ be/src/main/java/vn/fpt/diamond_shop/service/IOrderItemService.java
@@ -1,13 +1,14 @@
 package vn.fpt.diamond_shop.service;
 
 import vn.fpt.diamond_shop.model.entity.CartItem;
+import vn.fpt.diamond_shop.model.entity.Customer;
 import vn.fpt.diamond_shop.model.entity.Order;
 import vn.fpt.diamond_shop.model.entity.OrderItem;
 
 import java.util.List;
 
 public interface IOrderItemService {
-    void createOrderItemsByCartItems(List<CartItem> cartItems, Order order, int discount);
+    Order createOrderItemsByCartItems(List<CartItem> cartItems, Customer customer, int discount);
     List<OrderItem> getOrderItemsByOrder(Order order);
     List<Integer> getSaleStatistics();
     List<Integer> getJewelriesSaleStatistics();
